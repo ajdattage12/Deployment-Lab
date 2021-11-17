87d3d7b43adc75ca023ef8b7feca45f9d3851b64
Ariell Dattage <ariell.dattage@Ariells-MBP.localdomain>
2021-11-17 14:30:46 -0700

    trying again

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 
-// app.get("/js", function(req, res) {
-//   res.sendFile(path.join(__dirname, "../server/server.js"));
-// });
+app.get("/js", function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/index.js"));
+});
 
 const port = process.env.PORT || 4005;
 
@@ -13,6 +13,6 @@ app.listen(port, () => {
   console.log(`Docked at port ${port}`);
 });
 
-app.use("/js", express.static(path.join(__dirname, "../client/index.js")));
+// app.use("/js", express.static(path.join(__dirname, "../client/index.js")));
 // console.log("is this working")
 
